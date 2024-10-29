4eb60493931af4bfe747ca3f359bf0801b663629
Alogarithm_Ke <derrickmoio92@gmail.com>
2024-10-29 14:51:27 +0300

    learn oprenads operators and data types

---
+++ 30_Days_of_python/introduction.py
@@ -0,0 +1,32 @@
+print(3 + 4)
+print(3 * 4)
+print(3  / 4)
+print(3 ** 4)
+print(3  % 4)
+print(3  // 4)
+
+string = ['kim', 'moiben', 'Kenya', 'I am enjoyig 30 days of python']
+
+print(string)
+
+print(type(10))
+print(type(9.8))
+print(type(3.14))
+print(type(4 - 4j))
+print(type(['Asabeneh', 'Python', 'Finland']))
+print(type('kim'))
+print(type('moiben'))
+
+i = 10 - 2
+square1 = pow(i, 2)
+
+j = 8 -3
+square2 = pow(j, 2)
+
+k = square1 + square2
+
+import math
+
+l = math.sqrt(k)
+
+print(l)
