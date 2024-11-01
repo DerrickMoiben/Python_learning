b4d1263bb3ce278582c3042bd7ba16c70e9c7055
Alogarithm_Ke <derrickmoio92@gmail.com>
2024-11-01 12:50:02 +0300

    day 2 of relearning python

---
+++ 30_Days_of_python/variables.py
@@ -0,0 +1,26 @@
+#Day 2 of 30 days of python Programng
+
+first = 'Moiben'
+last = 'Derrick'
+
+full = 'Derrick Kimutai Moiben'
+
+country = 'Kenya'
+
+city = 'Kericho'
+
+age = 19
+
+year = 2005
+
+is_maried = 'Nop'
+
+is_true = True
+
+is_light_on = False
+
+multiple = {'kim', 3, 'Davis', 5, }
+
+print(type(multiple))
+
+
