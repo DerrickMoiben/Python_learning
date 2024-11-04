f28715af0c94e98e98cd0e3954a6270ec037cf77
Alogarithm_Ke <derrickmoio92@gmail.com>
2024-11-04 12:10:04 +0300

    just fucked up

---
+++ 30_Days_of_python/operators.py
@@ -0,0 +1,21 @@
+print('addition of', 4 + 5)
+
+a = 5
+b = 6
+
+total = a + b
+
+print('a + b', total)
+
+print('== Addition, Subtraction, Multiplication, Division, Modulus ==')
+
+exponetional = a ** b
+
+print(exponetional)
+
+r = 6
+import math
+
+r3 = 6 ** 2
+area = math.pi * 6 ** 2
+print(area)
--- 30_Days_of_python/variables.py
+++ 30_Days_of_python/variables.py
@@ -22,5 +22,53 @@ is_light_on = False
 multiple = {'kim', 3, 'Davis', 5, }
 
 print(type(multiple))
+print(len(first))
 
+num_one = 5
+num_two = 4
 
+total = num_one + num_two
+print(total)
+
+diff = num_one - num_two
+print(diff)
+
+product = num_one * num_two
+print(product)
+
+division = num_two / num_one
+print(division)
+
+rem = num_two % num_one
+print(rem)
+
+exp = pow(num_one, num_two)
+print(exp)
+
+print(diff)
+
+floor_division = num_one // num_two
+print(floor_division)
+
+import math
+flr = math.floor(num_one / num_two)
+print(flr)
+
+r = 30
+r2 = r * r
+
+area = math.pi * r2
+
+print(area)
+
+rd = r + r
+
+circum = math.pi * rd
+ans = round(circum, 3)
+
+print(ans)
+
+name = input('What is your first name: ')
+name2 = input('Enter you second name: ')
+country = input('Which country are you from: ')
+age = input('Please input your Age: ')
