f3b32bbd7acee445f97953909941d94146989888
Alogarithm_Ke <derrickmoio92@gmail.com>
2024-11-04 20:48:14 +0300

    learnt on upper and lower in python

---
+++ 30_Days_of_python/srtings.py
@@ -0,0 +1,22 @@
+a = 'Thirty '
+c = 'Days '
+d = 'of '
+e = 'Python'
+
+f = a  + c + d + e
+
+print(f)
+
+i = 'Coding '
+j =  'For '
+k = 'All'
+
+m = i + j + k
+print(m)
+
+company = m
+print(company)
+print(len(company))
+print(company.upper())
+b = company.lower()
+print(b)
