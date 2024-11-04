47f82b57df9d0b2903d261f0cbea48bb81f5c8dd
Alogarithm_Ke <derrickmoio92@gmail.com>
2024-11-04 12:31:52 +0300

    learnt casting in python

--- 30_Days_of_python/operators.py
+++ 30_Days_of_python/operators.py
@@ -19,3 +19,32 @@ import math
 r3 = 6 ** 2
 area = math.pi * 6 ** 2
 print(area)
+
+
+
+age = 40
+height  = 60
+i = 5 + 7j
+j = 6 + 5j
+
+sum = i + j
+
+print('Sum of this is how many', sum)
+print(sum)
+
+# Area of a  triangle
+base = int(input('Enter the base distance: '))
+height = int(input('Enter the height distance '))
+cal = base * height
+area = 0.5 * cal
+
+print('The area of the triangle ', area)
+
+# Perimeter of a triangle
+
+a = int(input('Enter the distance: '))
+b = int(input('Enter the distance of side b: '))
+c = int(input('Enter the distance for side c '))
+
+perimeter = a + b + c
+print('The perimeter is: ', perimeter)
