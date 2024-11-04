f451d3b778350a5f5fd991a6e68cf577c701a3e9
Alogarithm_Ke <derrickmoio92@gmail.com>
2024-11-04 20:32:42 +0300

    addeda pbl

--- 30_Days_of_python/operators.py
+++ 30_Days_of_python/operators.py
@@ -32,19 +32,53 @@ sum = i + j
 print('Sum of this is how many', sum)
 print(sum)
 
-# Area of a  triangle
-base = int(input('Enter the base distance: '))
-height = int(input('Enter the height distance '))
-cal = base * height
-area = 0.5 * cal
+# # Area of a  triangle
+# base = int(input('Enter the base distance: '))
+# height = int(input('Enter the height distance '))
+# cal = base * height
+# area = 0.5 * cal
 
-print('The area of the triangle ', area)
+# print('The area of the triangle ', area)
 
-# Perimeter of a triangle
+# # Perimeter of a triangle
 
-a = int(input('Enter the distance: '))
-b = int(input('Enter the distance of side b: '))
-c = int(input('Enter the distance for side c '))
+# a = int(input('Enter the distance: '))
+# b = int(input('Enter the distance of side b: '))
+# c = int(input('Enter the distance for side c '))
 
-perimeter = a + b + c
-print('The perimeter is: ', perimeter)
+# perimeter = a + b + c
+# print('The perimeter is: ', perimeter)
+
+# slope
+print(len('thon') == len('b'))
+print(len('kim') == len('kim'))
+
+print('jargon' in 'I hope this corse is not full of jargon')
+
+sentence = 'I hope thsi course is not full of jargon'
+print('jargon' in sentence)
+
+a = (1, 1, 1, 1, 1)
+b = [2, 1, 2, 4, 8]
+c = [3, 1, 3, 9, 27]
+d = [5, 1, 5, 25, 125]
+
+print(a)
+print(b)
+print(c)
+print(d)
+
+# Define the range of natural numbers
+n_values = range(1, 6)
+
+# Print the header
+print(f"{'n':<5}{'1':<5}{'n':<5}{'n^2':<5}{'n^3':<5}")
+
+# Generate and print the table rows
+for n in n_values:
+    print(f"{n:<5}{1:<5}{n:<5}{n**2:<5}{n**3:<5}")
+
+
+
+sentence = 'I hope thsi course is not full of jargon'
+print('jargn' in sentence)
