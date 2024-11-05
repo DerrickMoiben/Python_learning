f821d2641636eee68d80a37de6677bfb387e2a7b
Alogarithm_Ke <derrickmoio92@gmail.com>
2024-11-05 15:55:21 +0300

    learnig about list

--- 30_Days_of_python/list.py
+++ 30_Days_of_python/list.py
@@ -17,3 +17,25 @@ r = result // 2
 
 print(companies[r])
 print(companies[-1])
+companies[0] = 'X'
+print(companies)
+companies.append('IT')
+print(companies)
+companies.insert(r, 'IT')
+print(companies)
+two = companies[1]
+y = two.upper()
+print(y)
+print(companies)
+c = '#'.join(companies)
+print(c)
+find = 'IT' in companies
+print(find)
+
+
+sortedlist = sorted(companies, reverse=True)
+print(sortedlist)
+print(companies)
+
+slice1 = companies[3:8]
+print(slice1)
