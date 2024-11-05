863da3efb7eedb785568843f966721cf12e0b079
Alogarithm_Ke <derrickmoio92@gmail.com>
2024-11-05 14:44:47 +0300

    learnt about list

---
+++ 30_Days_of_python/list.py
@@ -0,0 +1,19 @@
+l = [1, 2, 3, 4, 5]
+
+print(len(l))
+one = l[0]
+middle = l[2]
+last = l[4]
+
+print(one, middle, last)
+
+mix = ['Moiben', 19, 6.0, 'confused', 13]
+companies = ['Facebook', 'Google', 'Microsoft', 'Apple', 'IBm', 'Oracle', 'Amazon']
+print(mix)
+result = len(companies)
+print(companies[0])
+
+r = result // 2
+
+print(companies[r])
+print(companies[-1])
