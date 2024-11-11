2eabcb8c1860c8539b52fcc1bbe9645db2a02fc6
Alogarithm_Ke <derrickmoio92@gmail.com>
2024-11-11 11:18:29 +0300

    learnig to the fullest

--- 30_Days_of_python/dic.py
+++ 30_Days_of_python/dic.py
@@ -1,1 +1,20 @@
-dog = {}
+dog = {
+    'name': 'Lasy',
+    'colour': 'blue',
+    'breed': 'Bulldog',
+    'legs': '4'
+}
+
+student = {
+    'first_name': 'kim',
+    'second_name': 'kimtai',
+    'third_name': 'moiben',
+    'geder': 'male',
+    'age': 250,
+    'skills': ['django', 'python', 'flask', 'html', 'c'],
+    'country':'kenya',
+    'address':{
+        'pobox': 13,
+        'zipcode': 34647
+    }
+    }
