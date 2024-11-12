aa6a66844816c5dd25048924ec4079eb61fc7141
Alogarithm_Ke <derrickmoio92@gmail.com>
2024-11-12 09:24:24 +0300

    nested conditionals

--- 30_Days_of_python/conditional.py
+++ 30_Days_of_python/conditional.py
@@ -1,7 +1,30 @@
-age = int(input('Enter you are age please'))
+# age = int(input('Enter you are age please'))
 
-if age >= 18:
-    print('Your are legal to drive a car ')
+# if age >= 18:
+#     print('Your are legal to drive a car ')
+# else:
+#     i = 18 - age
+#     print(f'What for {i} more years to drive')
+
+# me =  19
+# age = int(input('Enter your age my guy'))
+
+# dif =  age - me
+# if dif == 1:
+#     print(f'we have an age diffrence of {dif}')
+# if dif > 1:
+#     print(f'Your are {dif} older than me ')
+# else:
+#     print(age)
+#     print(me)
+
+
+one = int(input('Ennter the first number '))
+two = int(input('The second number '))
+
+if one > two:
+    print(F'{one } is greater than{two}')
+elif one == two:
+    print(f'{one} is equal to {two}')
 else:
-    i = 18 - age
-    print(f'What for {i} more years to drive')
+    print(f'{one} is less than {two}')
