2c064fc7c3af130dc01445901e2183f1ffe7975a
Alogarithm_Ke <derrickmoio92@gmail.com>
2024-11-12 07:56:11 +0300

    learning today conditional statements

---
+++ 30_Days_of_python/conditional.py
@@ -0,0 +1,7 @@
+age = int(input('Enter you are age please'))
+
+if age >= 18:
+    print('Your are legal to drive a car ')
+else:
+    i = 18 - age
+    print(f'What for {i} more years to drive')
--- 30_Days_of_python/email.py
+++
@@ -1,9 +0,0 @@
-def validate(email):
-    if email.count('@') == 1:
-        return True
-
-    return False
-
-email = input('Input a you email:')
-is_valid = validate(email)
-print('Is the email valud?', is_valid)
