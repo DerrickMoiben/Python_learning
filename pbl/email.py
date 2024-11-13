fea8a265369dc9d9ed519c8c9b7a1dcc1a59d51a
Alogarithm_Ke <derrickmoio92@gmail.com>
2024-11-13 21:58:44 +0300

    done we try again tommorroe

--- pbl/email.py
+++ pbl/email.py
@@ -5,12 +5,18 @@ def validate(email):
         position = email.find('@')
         if position != -1:
             recepint_name = email[:position]
-            checker  = re.compile(r'[a-zA-Z0-9](?!.*[._]){2}[a-zA_Z0-9._]{1,22}[a-zA-Z0-9]@[a-zA-Z0-9]+\.[a-zA-Z0-9-.]+$')
+            checker  = re.compile(r'^[a-zA-Z0-9](?!.*[._]{2})[a-zA-Z0-9._]{1,22}[a-zA-Z0-9]$')
             if checker.match(recepint_name):
-                return True
+                dot = email.find('.')
+                afterposition = position + 1
+                domain_name = email[afterposition:dot]
+                print(domain_name)
+                checkerfordomain = re.compile(r'^[a-zA-Z0-9-]{3,12}$')
+                if checkerfordomain.match(domain_name):
+                    return True
 
     return False
 
 email = input('Input a you email:')
 is_valid = validate(email)
-print('Is the email valud?', is_valid)
+print('Is the email valid?', is_valid)
