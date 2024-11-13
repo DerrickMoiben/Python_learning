984d5ee1aaa17d5300583ef5e6e80f912d7bd518
Alogarithm_Ke <derrickmoio92@gmail.com>
2024-11-13 10:06:02 +0300

    using the multiply operator

--- 30_Days_of_python/loops.py
+++ 30_Days_of_python/loops.py
@@ -2,11 +2,25 @@
 #     print(i)
 
 
-st = 0
+# st = 0
 
-while st < 10:
-    print(st)
-    st = st + 1
+# while st < 10:
+#     print(st)
+#     st = st + 1
 
-else:
-    print(st)
+# else:
+#     print(st)
+
+# numbers = [10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0]
+
+# for number in numbers:
+#     print(number)
+
+# st = 10
+# while st > 0:
+#     print(st)
+#     st = st - 1
+
+
+for i in range(0, 8):
+    print('#' * i)
