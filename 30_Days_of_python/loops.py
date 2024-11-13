333b163a8f8ec92c66ec4051daf70a57a4f12ee1
Alogarithm_Ke <derrickmoio92@gmail.com>
2024-11-13 09:26:09 +0300

    while loop

--- 30_Days_of_python/loops.py
+++ 30_Days_of_python/loops.py
@@ -1,24 +1,12 @@
-# count = 0
-# while count < 5:
-#     print(count)
-#     count = count + 1
-# else:
-#     print(count)
+# for i in (range(11)):
+#     print(i)
 
 
+st = 0
 
-# language = 'Python'
-# for letter in language:
-#     print(letter)
+while st < 10:
+    print(st)
+    st = st + 1
 
-
-# for i in range(len(language)):
-#     print(language[i])
-
-numbers = (0,1,2,3,4,5)
-for number in numbers:
-    print(number)
-#     if number == 3:
-#         continue
-#     print('Next number should be ', number + 1) if number != 5 else print("loop's end") # for short hand conditions need both if and else statements
-# print('outside the loop')
+else:
+    print(st)
