58bd5088c90cc1b7406ee175a7a30ae31919aae1
Alogarithm_Ke <derrickmoio92@gmail.com>
2024-11-13 11:27:48 +0300

    almost done

--- 30_Days_of_python/loops.py
+++ 30_Days_of_python/loops.py
@@ -22,5 +22,63 @@
 #     st = st - 1
 
 
-for i in range(0, 8):
-    print('#' * i)
+# for i in range(0, 8):
+#     print('#' * i)
+
+
+# y = 'kim'
+# for x in y:
+#     for t in x:
+#         print(t)
+
+
+# for i in range(9):
+#     print('#########')
+
+
+for i in range(9):
+    for j in range(9):
+        print('#', end='')
+
+    print()
+
+# for i in range(0, 11):
+#     print(i)
+#     for j in range(11):
+#         print (f'{i} * {j} = {i * j}')
+# print()
+
+
+for i in range(11):
+    print(f'{i} * {i} = {i * i}')
+
+
+ls =  ['Python', 'Numpy','Pandas','Django', 'Flask']
+
+for item in ls:
+    print(item)
+
+even = 0
+odd = 0
+for i in range(0, 100):
+    if i % 2 == 0:
+        even = even + 1
+        print('This is an even number', i)
+    else:
+        odd = odd + 1
+        print('THis is an odd number', i)
+print(f'The sum of even numbers is {even} while the numbers of the odd numbers is {odd}')
+
+
+sum = 0
+even = 0
+odd = 0
+for i in range(0, 101):
+    sum = sum + i
+    if i % 2 == 0:
+        even = even + i
+    else:
+        odd = odd + i
+    
+print('The sum of all numbers is', sum)
+print(f'The sum of even numbers is {even}, while the sum of odd numbers is {odd}')
