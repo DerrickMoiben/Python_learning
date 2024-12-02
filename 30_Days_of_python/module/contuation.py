4f4442450989272549f0708e30c8c4e34d8210ca
Alogarithm_Ke <derrickmoio92@gmail.com>
2024-12-02 12:42:51 +0300

    having complication o this part

--- 30_Days_of_python/module/contuation.py
+++ 30_Days_of_python/module/contuation.py
@@ -24,12 +24,31 @@ def random_user_id():
         
 print(random_user_id())
         
-        
+
+from math import *    
         
 def user_id_gen_by_user():
-    number = input('Enter the number of id you need ')
-    character = input('Enter the number of characters you need in your id ')
+    number = int(input('Enter the number of id you need '))
+    ids = int(input('Enter the number of characters you need in your id '))
+    for i in range(ids):
+        id = ""
+        div = number / 2
+        times = ceil(div)
+        for i in range(times):
+            digits = str(randint(0, times))
+            letters = choice(string.ascii_letters)
+            id += digits + letters
+            
+    return f"#{id}"
 
-
-user_id_gen_by_user()
+print(user_id_gen_by_user())
     
+    
+def rgb_color_gen():
+    color1 =  randint(0, 255)
+    color2 =  randint(0, 225)
+    color3 =  randint(0, 255)
+    
+    return f'RGB{color1}, {color2}, {color3}'
+
+print(rgb_color_gen())    
