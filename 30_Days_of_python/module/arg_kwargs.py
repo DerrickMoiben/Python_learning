858847786ff6814df8ce1344eec4c147b61b9159
DerrickMoiben <derrickmoio92@gmail.com>
2025-05-19 16:55:44 +0300

    just understood the meaning of args and kwargs

---
+++ 30_Days_of_python/module/arg_kwargs.py
@@ -0,0 +1,5 @@
+def example(*args, **kwargs):
+    print("The  args are the following ", args)
+    print("The  kwargs are the following ", kwargs)
+
+example (1, 2,  name="KIMUtai", age=23, country="Kenya")
